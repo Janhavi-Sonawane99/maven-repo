383373425474901bebccd0ba33691a5358ce56f9
Janhavi-Sonawane99 <janhavisonawane33@gmail.com>
2020-12-21 15:01:01 +0530

    added method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -6,9 +6,13 @@ public class Calc {
 	public static int add(int num1,int num2) {
 		return num1+num2;
 	}
+	public static int sub(int num1,int num2) {
+		return num1-num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
+		System.out.println(sub(60,40));
 
 	}
 
