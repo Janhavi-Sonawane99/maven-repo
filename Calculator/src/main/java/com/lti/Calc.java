88bd270308e4c135cbf8b6db354843b0deeb5add
Janhavi-Sonawane99 <janhavisonawane33@gmail.com>
2020-12-21 15:10:02 +0530

    method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -9,11 +9,15 @@ public class Calc {
 	public static int sub(int num1,int num2) {
 		return num1-num2;
 	}
+	public static int prod(int num1,int num2) {
+		return num1*num2;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
 		System.out.println(sub(60,40));
-
+        System.out.println(prod(3,4));
 	}
 
 }
