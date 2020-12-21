8a8300114ad5df4aa7251d655291d567cc1d1b89
Janhavi-Sonawane99 <janhavisonawane33@gmail.com>
2020-12-21 15:16:29 +0530

    div added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -12,12 +12,16 @@ public class Calc {
 	public static int prod(int num1,int num2) {
 		return num1*num2;
 	}
+	public static int div(int num1,int num2) {
+		return num1/num2;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(10,20));
 		System.out.println(sub(60,40));
         System.out.println(prod(3,4));
+        System.out.println(div(10,2));
 	}
 
 }
